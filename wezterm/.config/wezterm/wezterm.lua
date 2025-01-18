4ba5dd4faa3f1921823048bf7cf7ff29e3fd9bfc
Michał Danaj <michal.danaj@gmail.com>
2025-01-18 18:00:17 +0100

    Dodanie do wezterm ctrl+f ctrl+b

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -13,36 +13,77 @@ local config = wezterm.config_builder()
 --config.color_scheme = 'Catppuccin Mocha'
 config.color_scheme = 'Chalk (Gogh)'
 
-config.window_background_opacity = 0.97
+config.window_background_opacity = 0.95
 
 local act = wezterm.action
 
 --   domyślne przydatne skróty   ---
 -- ctrl + shift + z - przełącza pane na pełen ekran
 --
---
---
-
 config.keys = {
-  -- { key = 't', mods = 'ALT', action = wezterm.action.ShowTabNavigator },
-  { key = '|', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal
-        { domain = 'CurrentPaneDomain' }, },
-  { key = '_', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical
-        { domain = 'CurrentPaneDomain' }, },
-  { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentPane
-        { confirm = true }, },
-  { key = 'W', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentPane
-        { confirm = true }, },
-  { key = 't', mods = 'CTRL', action = act.SpawnTab 'DefaultDomain', },
-  -- Create a new tab in the default domain
-  { key = 'T', mods = 'CTRL|SHIFT', action = act.SpawnTab 'CurrentPaneDomain' },
-  { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
-  { key = 'b', mods = 'CTRL', action = act.ScrollByPage(-1) },
-  { key = 'f', mods = 'CTRL', action = act.ScrollByPage(1) },
-  { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left', },
-  { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right', },
-  { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up', },
-  { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down', },
+    -- { key = 't', mods = 'ALT', action = wezterm.action.ShowTabNavigator },
+    {
+        key = '|',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitHorizontal
+            { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = '_',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitVertical
+            { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'w',
+        mods = 'CTRL',
+        action = wezterm.action.CloseCurrentPane
+            { confirm = true },
+    },
+    {
+        key = 'W',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.CloseCurrentPane
+            { confirm = true },
+    },
+    { key = 't', mods = 'CTRL',       action = act.SpawnTab 'DefaultDomain', },
+    -- Create a new tab in the default domain
+    { key = 'T', mods = 'CTRL|SHIFT', action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 'l', mods = 'ALT',        action = wezterm.action.ShowLauncher },
+    -- Aby w wezterm było takie przewijanie strony jak w vim, czyli ctrl+f,
+    -- ctrl+b, ale równocześnie klawisze były przekazane do vim gdy jest
+    -- uruchomiony, potrzebna jest taka jak poniżej konstrukcja
+    {
+        key = 'f',
+        mods = 'CTRL',
+        action = wezterm.action_callback(function(win, pane)
+            print(pane:is_alt_screen_active())
+            if pane:is_alt_screen_active() then
+                win:perform_action(wezterm.action.SendKey { key = 'f', mods = 'CTRL' }, pane)
+            else
+                win:perform_action(wezterm.action.ScrollByPage(1), pane)
+            end
+        end)
+    },
+    {
+        key = 'b',
+        mods = 'CTRL',
+        action = wezterm.action_callback(function(win, pane)
+            print(pane:is_alt_screen_active())
+            if pane:is_alt_screen_active() then
+                win:perform_action(wezterm.action.SendKey { key = 'b', mods = 'CTRL' }, pane)
+            else
+                win:perform_action(wezterm.action.ScrollByPage(-1), pane)
+            end
+        end)
+    },
+    -- { key = 'b', mods = 'CTRL', action = act.ScrollByPage(-1) },
+    --{ key = 'b', mods = 'CTRL', action = test{key='b', mods='CTRL', action=wezterm.action.ScrollByPage(-1)} },
+    --{ key = 'f', mods = 'CTRL', action = act.ScrollByPage(1) },
+    { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Left', },
+    { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Right', },
+    { key = 'k', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Up', },
+    { key = 'j', mods = 'CTRL|SHIFT', action = act.ActivatePaneDirection 'Down', },
 }
 
 -- Spawn a fish shell in login mode
@@ -61,32 +102,32 @@ wezterm.on('update-right-status', function(window, pane)
         local hostname = ''
 
         if type(cwd_uri) == 'userdata' then
-        -- Running on a newer version of wezterm and we have
-        -- a URL object here, making this simple!
+            -- Running on a newer version of wezterm and we have
+            -- a URL object here, making this simple!
 
-        cwd = cwd_uri.file_path
-        hostname = cwd_uri.host or wezterm.hostname()
+            cwd = cwd_uri.file_path
+            hostname = cwd_uri.host or wezterm.hostname()
         else
-        -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
-        -- which doesn't have the Url object
-        cwd_uri = cwd_uri:sub(8)
-        local slash = cwd_uri:find '/'
-        if slash then
-            hostname = cwd_uri:sub(1, slash - 1)
-            -- and extract the cwd from the uri, decoding %-encoding
-            cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
-            return string.char(tonumber(hex, 16))
-            end)
-        end
+            -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+            -- which doesn't have the Url object
+            cwd_uri = cwd_uri:sub(8)
+            local slash = cwd_uri:find '/'
+            if slash then
+                hostname = cwd_uri:sub(1, slash - 1)
+                -- and extract the cwd from the uri, decoding %-encoding
+                cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+                    return string.char(tonumber(hex, 16))
+                end)
+            end
         end
 
         -- Remove the domain name portion of the hostname
         local dot = hostname:find '[.]'
         if dot then
-        hostname = hostname:sub(1, dot - 1)
+            hostname = hostname:sub(1, dot - 1)
         end
         if hostname == '' then
-        hostname = wezterm.hostname()
+            hostname = wezterm.hostname()
         end
 
         table.insert(cells, cwd)
@@ -131,8 +172,8 @@ wezterm.on('update-right-status', function(window, pane)
         table.insert(elements, { Background = { Color = colors[cell_no] } })
         table.insert(elements, { Text = ' ' .. text .. ' ' })
         if not is_last then
-        table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
-        table.insert(elements, { Text = SOLID_LEFT_ARROW })
+            table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+            table.insert(elements, { Text = SOLID_LEFT_ARROW })
         end
         num_cells = num_cells + 1
     end
