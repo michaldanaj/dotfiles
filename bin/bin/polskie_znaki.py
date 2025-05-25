07fcef3ef9464018aafc9fd4920a9268843d2447
michaldanaj <mail@moj.pl>
2025-05-25 09:33:15 +0200

    bin: dodanie kolejnej transformacji w konwersji znaków

--- bin/bin/polskie_znaki.py
+++ bin/bin/polskie_znaki.py
@@ -3,19 +3,23 @@ import sys
 
 
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Użycie: ./napraw_kodowanie.py <plik_wejsciowy> <plik_wyjsciowy>")
         sys.exit(1)
 
     input_filename = sys.argv[1]
-    output_filename = sys.argv[2]
+    # output_filename = sys.argv[2]
+    output_filename = "popr_" + input_filename
 
     try:
         with open(input_filename, "rb") as file:
             data = file.read()
 
         # Naprawa kodowania
-        fixed_data = data.decode("utf8").encode("cp1250").decode("utf8")
+        if data[0:2] == b"\xcb\x99":
+            fixed_data = data.decode("utf8").encode("cp1250").decode("utf-16-le")
+        else:
+            fixed_data = data.decode("utf8").encode("cp1250").decode("utf8")
 
         with open(output_filename, "w", encoding="utf8") as file:
             file.write(fixed_data)
